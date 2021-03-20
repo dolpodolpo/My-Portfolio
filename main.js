9df4e17d2f8678fe46ff361cd99822fcb036cc5c
dolpodolpo <adolfrenzmontaner@gmail.com>
2021-03-20 09:13:29 +0800

    Added Files

---
+++ main.js
@@ -0,0 +1,6 @@
+function openEmail() {
+  document.getElementById("mailId").classList.add("showEmail");
+
+  document.getElementById("mailIdText").innerHTML =
+    "Send me an email at adolfrenzmontaner@gmail.com";
+}
